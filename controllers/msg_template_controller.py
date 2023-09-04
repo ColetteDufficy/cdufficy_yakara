c3eef0fa5205c4aa3e2881965f712a156212d71c
C.Dufficy <c.dufficy@gmail.com>
2023-09-04 11:16:32 +0100

    final clean up

--- controllers/msg_template_controller.py
+++ controllers/msg_template_controller.py
@@ -23,8 +23,8 @@ messages_blueprint = Blueprint("index", __name__)
 # 7. DELETE
 
 # NEW
-# GET '/' requesting all msg_title so they appear in drop down as options
-# POST '/' onchange event prompts POST response to fill preview screen with user-choosen msg_content
+# GET '/' requesting all msg_titles so they appear in drop down as options
+# POST '/' onchange event in HTML, prompts POST response to fill preview screen with corresponding msg_content
 @messages_blueprint.route("/", methods=['GET', 'POST'])
 def get_all_msg_templates():
     selected_template_msg_content = ""
@@ -47,6 +47,8 @@ def get_all_msg_templates():
     return render_template("index.html", msg_templates=msg_templates, selected_template_msg_content=selected_template_msg_content, selected_template_id=selected_template_id)
 
 
+
+
 # CREATE
 # POST 'msg_sent' back to db as persistent data
 @messages_blueprint.route("/sent", methods=['POST'])
@@ -55,24 +57,23 @@ def create_msg_sent_to_db():
     recipient_email = request.form['email']
     msg_template_id = request.form['msg_template_id']
 
-    # Perform validation
+    # form validation
     if not recipient_name or not recipient_email or not msg_template_id:
-        # Validation failed, redirect back to the error page
+        # if validation has failed, redirect back to the error.html page
         return render_template("error.html")
     
     # Check for a valid email format using regular expression
     if not re.match(r"[^@]+@[^@]+\.[^@]+", recipient_email):
-        return render_template("error.html", error="Invalid email format")
+        return render_template("error.html")
 
-
-
+    # capturing the msg_template under varibale name msg_template so Msg_sent instance can be generated and sent back to db 
     msg_template = msg_template_repository.select(msg_template_id)
 
+    #being create and then saved to db
     msg_sent = Msg_sent(recipient_name, recipient_email, msg_template)
     sent_msg_repository.save(msg_sent)
 
     # print("msg_sent submiussion:", msg_sent)
-
 
     return render_template("sent.html")
 
