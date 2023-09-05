56fe7bcfadfe7aeddaff4ef15ed23b9629fd73e9
C.Dufficy <c.dufficy@gmail.com>
2023-09-05 10:12:18 +0100

    feature branch started app.config set up

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template
+from flask_mail import Mail, Message
 
 from controllers.msg_template_controller import messages_blueprint
 
 app = Flask(__name__)
+
+
+
+app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = 'b33a25b498cb37'
+app.config['MAIL_PASSWORD'] = '261eddc56c000d'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+
+# app.config['MAIL_SERVER']='smtp.gmail.com'
+# app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USERNAME'] = 'c.dufficy@gmail.com'
+# app.config['MAIL_PASSWORD'] = '6Jellies!G'
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 
 app.register_blueprint(messages_blueprint)
